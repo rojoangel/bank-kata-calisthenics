0317190cc9f131eb25f47b7fd1ad85714ddb61d1
Angel Rojo Marijuan <rojo.angel@gmail.com>
2022-09-25 00:52:25 +0200

    remove unused method

--- src/main/java/bank/Statement.java
+++ src/main/java/bank/Statement.java
@@ -1,18 +1,12 @@
 package bank;
 
 import java.io.PrintStream;
-import java.math.BigDecimal;
-import java.util.Date;
 
 public class Statement {
     private static final String HEADER = "| date       | credit  | debit  | balance |";
 
     public void printTo(PrintStream printer) {
         printer.println(HEADER);
-    }
-
-    public void addLine(Money amount, Date date, Money balance) {
-
     }
 
     public void addLine(Transaction transaction, Money balance) {
