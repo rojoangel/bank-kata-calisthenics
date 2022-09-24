026602a07ea2b96ee7930ce61ca3c84613f1102b
Angel Rojo Marijuan <rojo.angel@gmail.com>
2022-09-24 12:39:12 +0200

    add unimplemented steps

--- src/test/java/bank/StepDefinitions.java
+++ src/test/java/bank/StepDefinitions.java
@@ -18,4 +18,36 @@ public class StepDefinitions {
     public void theScenarioPasses() {
     }
 
+    @Given("a client makes a deposit of {int} on {int}-{int}-{int}")
+    public void a_client_makes_a_deposit_of_on(Integer int1, Integer int2, Integer int3, Integer int4) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("a deposit of {int} on {int}-{int}-{int}")
+    public void a_deposit_of_on(Integer int1, Integer int2, Integer int3, Integer int4) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("a withdrawal of {int} on {int}-{int}-{int}")
+    public void a_withdrawal_of_on(Integer int1, Integer int2, Integer int3, Integer int4) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("she prints her bank statement")
+    public void she_prints_her_bank_statement() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("she would see")
+    public void she_would_see(io.cucumber.datatable.DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new io.cucumber.java.PendingException();
+    }
+
 }
