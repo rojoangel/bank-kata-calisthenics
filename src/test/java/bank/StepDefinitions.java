483da56beaadbda6d8b0533851404258f12beb67
Angel Rojo Marijuan <rojo.angel@gmail.com>
2022-09-24 12:40:01 +0200

    remove example feature

--- src/test/java/bank/StepDefinitions.java
+++ src/test/java/bank/StepDefinitions.java
@@ -1,43 +1,34 @@
 package bank;
 
-import io.cucumber.java.en.*;
-
-import org.junit.jupiter.api.Assertions.*;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class StepDefinitions {
-
-    @Given("an example scenario")
-    public void anExampleScenario() {
-    }
-
-    @When("all step definitions are implemented")
-    public void allStepDefinitionsAreImplemented() {
-    }
-
-    @Then("the scenario passes")
-    public void theScenarioPasses() {
-    }
-
     @Given("a client makes a deposit of {int} on {int}-{int}-{int}")
     public void a_client_makes_a_deposit_of_on(Integer int1, Integer int2, Integer int3, Integer int4) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+
     @Given("a deposit of {int} on {int}-{int}-{int}")
     public void a_deposit_of_on(Integer int1, Integer int2, Integer int3, Integer int4) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+
     @Given("a withdrawal of {int} on {int}-{int}-{int}")
     public void a_withdrawal_of_on(Integer int1, Integer int2, Integer int3, Integer int4) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+
     @When("she prints her bank statement")
     public void she_prints_her_bank_statement() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
+
     @Then("she would see")
     public void she_would_see(io.cucumber.datatable.DataTable dataTable) {
         // Write code here that turns the phrase above into concrete actions
