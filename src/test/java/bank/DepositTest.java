067037c708e9000531671a03766d25a3cf25ef22
Angel Rojo Marijuan <rojo.angel@gmail.com>
2022-09-25 00:51:20 +0200

    refactor

--- src/test/java/bank/DepositTest.java
+++ src/test/java/bank/DepositTest.java
@@ -5,15 +5,16 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DepositTest {
 
     @Test
     void should_statically_initialize_a_deposit() {
+        Date now = new Date();
         assertEquals(
-                new Deposit(Money.moneyOf(BigDecimal.TEN), new Date()),
-                Deposit.depositOf(Money.moneyOf(BigDecimal.TEN), new Date())
+                new Deposit(Money.moneyOf(BigDecimal.TEN), now),
+                Deposit.depositOf(Money.moneyOf(BigDecimal.TEN), now)
         );
     }
 }
--- src/test/java/bank/WithdrawalTest.java
+++ src/test/java/bank/WithdrawalTest.java
@@ -7,15 +7,16 @@ import java.util.Date;
 import static bank.Money.moneyOf;
 import static bank.Withdrawal.withdrawalOf;
 import static java.math.BigDecimal.TEN;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class WithdrawalTest {
 
     @Test
     void should_statically_build_a_withdrawal() {
+        Date now = new Date();
         assertEquals(
-                new Withdrawal(moneyOf(TEN), new Date()),
-                withdrawalOf(moneyOf(TEN), new Date())
+                new Withdrawal(moneyOf(TEN), now),
+                withdrawalOf(moneyOf(TEN), now)
         );
     }
 }
